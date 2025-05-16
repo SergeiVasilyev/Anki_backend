6015211018e50d8804cfe46efa9aeefe66db2f02
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2025-05-16 10:16:45 +0300

    Change port 8000 to 8880

--- runserver_uvicorn.bat
+++ runserver_uvicorn.bat
@@ -4,7 +4,7 @@
 call .\venv\Scripts\activate
 
 :: Run server
-uvicorn canellus.asgi:application --reload
+uvicorn canellus.asgi:application --host 127.0.0.1 --port 8880 --reload
 
 :: Run server prod mode
 @REM uvicorn canellus.asgi:application --host 127.0.0.1 --port 8000 --workers 4
